423f2bf483de3a3099ea5fb1d448e16e67b2a9c6
cebolix00 <35338488+cebolix00@users.noreply.github.com>
2024-10-23 21:59:06 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/power-flow-card.js',
+  input: 'src/sofar-power-flow-card.js',  // Update this line
   output: {
     dir: 'dist',
     format: 'es',
+    filename: 'sofar-power-flow-card.js'  // Make sure this matches
   },
   plugins: [
     resolve(),
